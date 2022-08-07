90820b2885a12540ce685c6c68fa557cd99f906f
edegp <a.yuhi1164@gmail.com>
2022-08-07 11:30:36 +0900

    antd componentsに変更

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -33,19 +33,6 @@ export default function Home({ blogs }) {
       </section>
     </Layout>
   );
-  // return (
-  //   <div>
-  //     <ul>
-  //       {blog.map((blog) => (
-  //         <li key={blog.id}>
-  //           <Link href={`/blog/${blog.id}`}>
-  //             <a>{blog.title}</a>
-  //           </Link>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
--- pages/contact/examples.tsx
+++ pages/contact/examples.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Link from "next/link";
 import Layout from "../../components/Layout";
+import { List, Typography } from "antd";
 
 export default function Index() {
   return (
@@ -10,17 +11,15 @@ export default function Index() {
     >
       <section className="section">
         <div className="container">
-          <div className="content">
-            <h1>Hi people</h1>
-            <p>
-              これは、microCMSのフォーム処理とNextを統合したサイトの例です。
-            </p>
-            <ul>
-              <li>
-                <Link href="/contact">基本お問い合わせフォーム</Link>
-              </li>
-            </ul>
-          </div>
+          <Typography.Title level={2}>こんにちは！！</Typography.Title>
+          <Typography.Paragraph>
+            これは、microCMSのフォーム処理とNextを統合したサイトの例です。
+          </Typography.Paragraph>
+          <List>
+            <List.Item>
+              <Link href="/contact">基本お問い合わせフォーム</Link>
+            </List.Item>
+          </List>
         </div>
       </section>
     </Layout>
