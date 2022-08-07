117354b271637910b81aaac880ebb8b8fe235df8
edegp <a.yuhi1164@gmail.com>
2022-08-07 11:28:35 +0900

    example 修正

--- pages/contact/examples.tsx
+++ pages/contact/examples.tsx
@@ -2,28 +2,27 @@ import * as React from "react";
 import Link from "next/link";
 import Layout from "../../components/Layout";
 
-export default function Index () {
-    return (
-      <Layout
-        title="例"
-        description="これは、microCMSのフォーム処理とNextを統合したサイトの例です。"
-      >
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Hi people</h1>
-              <p>
-                これは、microCMSのフォーム処理とNextを統合したサイトの例です。
-              </p>
-              <ul>
-                <li>
-                  <Link href="/contact">基本お問い合わせフォーム</Link>
-                </li>
-              </ul>
-            </div>
+export default function Index() {
+  return (
+    <Layout
+      title="例"
+      description="これは、microCMSのフォーム処理とNextを統合したサイトの例です。"
+    >
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <h1>Hi people</h1>
+            <p>
+              これは、microCMSのフォーム処理とNextを統合したサイトの例です。
+            </p>
+            <ul>
+              <li>
+                <Link href="/contact">基本お問い合わせフォーム</Link>
+              </li>
+            </ul>
           </div>
-        </section>
-      </Layout>
-    );
-  }
+        </div>
+      </section>
+    </Layout>
+  );
 }
